510dcb7cc7ffe2a7bd2eb4c0c5150827512e6319
Jana Křelinová <jana.krelinova@tul.cz>
2025-10-02 14:05:35 +0200

    #Výpis řady z první hodnoty

---
+++ IS-Programy/Program001-vypis-rady/Program.cs
@@ -0,0 +1,34 @@
+﻿
+string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("**********************************");
+    Console.WriteLine("********Výpis číselné řady********");
+    Console.WriteLine("**********************************");
+    Console.WriteLine("********Jana Křelinová************");
+    Console.WriteLine("*********02.10.2025***************");
+    Console.WriteLine("**********************************");
+    Console.WriteLine();
+
+    //Vstup hodnoty do programu, ale špatně řešený
+    //Console.Write("Zadejte první čislo řady")
+    //int first = int.Parse(Console.ReadLine());
+
+    //Vstup do programu řešený lépe
+    Console.Write("Zadejte první číslo řady(celé číslo)")
+    int first;
+    while (!int.TryParse(Console.ReadLine(), out first))
+    {
+        Console.WriteLine("Nezadali jste celé číslo. Zadejte první číslo řady znovu")
+
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}
+
+
+

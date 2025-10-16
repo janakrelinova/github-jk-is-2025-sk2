bb584e882ca697d5c1ca1cf69f3703dbc379c54e
Jana Křelinová <jana.krelinova@tul.cz>
2025-10-16 18:34:40 +0200

    #5 soucet cifer - jine reseni

---
+++ IS-Programy/Program002b-soucet-cifer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Zadej celé číslo: ");
+        string vstup = Console.ReadLine();
+
+        int soucet = 0;
+
+        foreach (char znak in vstup)
+        {
+            if (char.IsDigit(znak))
+            {
+                int cifra = int.Parse(znak.ToString());
+                soucet += cifra;
+            }
+        }
+
+        Console.WriteLine($"Součet cifer je: {soucet}");
+    }
+}

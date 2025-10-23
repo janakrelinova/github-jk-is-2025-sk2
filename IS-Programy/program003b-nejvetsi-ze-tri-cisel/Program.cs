aec9f8b375a8417aaec5a83fb80a6254bb81e449
Jana Křelinová <jana.krelinova@tul.cz>
2025-10-23 13:36:57 +0200

    #6 nejvetsi cislo - moje reseni

---
+++ IS-Programy/program003b-nejvetsi-ze-tri-cisel/Program.cs
@@ -0,0 +1,64 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("***********Seřazení dvou čísel *************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Jana Křelinová ******************");
+    Console.WriteLine("************** 23.10.2025 *******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+
+    Console.Write("Zadejte celé číslo A: ");
+    int a;
+    while (!int.TryParse(Console.ReadLine(), out a))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte celé číslo znovu A: ");
+    }
+    
+    Console.Write("Zadejte celé číslo B: ");
+    int b;
+    while (!int.TryParse(Console.ReadLine(), out b))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte celé číslo znovu B: ");
+    }
+
+    Console.Write("Zadejte celé číslo C: ");
+    int c;
+    while (!int.TryParse(Console.ReadLine(), out c))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte celé číslo znovu C: ");
+    }
+
+    Console.WriteLine();
+
+    if (a > b)
+        if (a > c)
+        {
+            Console.WriteLine($"Největší číslo je {a} ");
+        }
+
+    if (b > a)
+        if (b > c)
+        {
+            Console.WriteLine($"Největší číslo je {b}");
+        }
+        
+    if(c > a)
+        if(c > b)
+        {
+            Console.WriteLine($"Největší číslo je {c}");
+        }
+
+
+
+
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}
+

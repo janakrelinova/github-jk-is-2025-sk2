7e5c6e0046b9c20aeebd7ad9d26aeacbb7b2e28a
Jana Křelinová <jana.krelinova@tul.cz>
2025-10-25 12:19:07 +0200

    #7 prvni obrazec

---
+++ IS-Programy/program004c-obrazec-1/Program.cs
@@ -0,0 +1,53 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("**** vykreslení obrazce - šachovnice *******");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Jana Křelinová ***************");
+    Console.WriteLine("************** 25.10.2025 ******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+
+
+  Console.Write("Zadejte šířku obrazce: ");
+    int width;
+    while (!int.TryParse(Console.ReadLine(), out width))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte šířku obdélníka znovu: ");
+    }
+
+    Console.Write("Zadejte výšku obrazce: ");
+    int height;
+    while (!int.TryParse(Console.ReadLine(), out height))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte výšku obdélníka znovu: ");
+    }
+
+
+    for (int i = 1; i <= height; i++)
+    {
+        for (int j = 1; j <= width; j++)
+        {
+            if ((i + j) % 2 == 0)
+                Console.Write("**");
+            else
+                Console.Write("  ");
+        }
+        Console.WriteLine();
+    }  
+    
+    
+    
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}
+

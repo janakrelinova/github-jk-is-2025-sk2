5a88d85bfb97cfcd6af8dd7e70f828ba42c9df11
Jana Křelinová <jana.krelinova@tul.cz>
2025-11-06 13:36:49 +0100

    #8 generator

---
+++ IS-Programy/program005-generator/Program.cs
@@ -0,0 +1,70 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("***** Generátor pseudonáhodných čísel ******");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************* Jana Křelinová ***************");
+    Console.WriteLine("************** 06.11.2025 ******************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+
+    Console.Write("Zadajte počet čísel (celé číslo): ");
+    int n;
+    while(!int.TryParse(Console.ReadLine(), out n))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte počet čísel znovu");
+    }
+
+    Console.Write("Zadajte dolní mez (celé číslo): ");
+    int lowerBound; //dolní mez
+    while(!int.TryParse(Console.ReadLine(), out lowerBound))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte dolní mez znovu");
+    }
+
+    Console.Write("Zadajte horní mez (celé číslo): ");
+    int upperBound; //horní mez
+    while (!int.TryParse(Console.ReadLine(), out upperBound))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte horní mez znovu");
+    }
+
+    Console.WriteLine();
+    Console.WriteLine("======================");
+    Console.WriteLine("Zadané hodnoty");
+    Console.WriteLine("Počet čísel: {0}, Dolní mez: {1}, Horní mez: {2}", n, lowerBound, upperBound);
+    //pokud by byla v {} všude 0, vypsalo by se všude jen "n"
+    Console.WriteLine("======================");
+    Console.WriteLine();
+
+    // deklarace pole (array)
+    int[] myRandNumbs = new int[n];
+
+    // příprava pro využití třídy Random
+    //Random myRandNumb = new Random();
+    Random myRandNumb = new Random(15); //seed - semínko
+
+
+    Console.WriteLine();
+    Console.WriteLine("Náhodná čísla: ");
+
+    for(int i = 0; i < n ; i++)
+    {
+        myRandNumbs[i] = myRandNumb.Next(lowerBound, upperBound);
+        Console.Write("{0}; ", myRandNumbs[i]);
+    }
+
+
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}
+

00c70057ca4cc94a9154059c82bab4c1a6c60cae
Jana Křelinová <jana.krelinova@tul.cz>
2025-11-06 14:03:40 +0100

    #8 generator rozsireni

--- IS-Programy/program005-generator/Program.cs
+++ IS-Programy/program005-generator/Program.cs
@@ -46,21 +46,60 @@ while (again == "a")
     int[] myRandNumbs = new int[n];
 
     // příprava pro využití třídy Random
-    //Random myRandNumb = new Random();
-    Random myRandNumb = new Random(15); //seed - semínko
+    Random myRandNumb = new Random();
+    //Random myRandNumb = new Random(15); //seed - semínko
+
+    //kladná, záporná nebo nuly
+    int negativeNumbs = 0;
+    int positiveNumbs = 0;
+    int zeros = 0;
+
+    //sudá nebo lichá
+    int evenNumbs = 0;
+    int oddNumbs = 0;
+
+
 
 
     Console.WriteLine();
     Console.WriteLine("Náhodná čísla: ");
+    Console.WriteLine();
 
-    for(int i = 0; i < n ; i++)
+    for (int i = 0; i < n; i++)
     {
         myRandNumbs[i] = myRandNumb.Next(lowerBound, upperBound);
         Console.Write("{0}; ", myRandNumbs[i]);
+
+        /*
+            if (myRandNumbs[i] > 0)
+                positiveNumbs++; //když je číslo větší než 0, positiveNumbs zvýšíme o 1
+            if (myRandNumbs[i] < 0)
+                negativeNumbs--;
+            if (myRandNumbs[i] == 0)
+                zeros++; */
+
+        if (myRandNumbs[i] > 0)      // program nemusí projít celým souborem porovnávání - když je číslo kladné, nepokračuje dál
+            positiveNumbs++;
+        else if (myRandNumbs[i] < 0)
+            negativeNumbs++;
+        else
+            zeros++;
+
+        if (myRandNumbs[i] % 2 == 0)
+            evenNumbs++;
+        else
+            oddNumbs++;
+
+
+
     }
 
-
-
+    Console.WriteLine();
+    Console.WriteLine("Počet kladných {0}", positiveNumbs);
+    Console.WriteLine("Počet záporných: {0}", negativeNumbs);
+    Console.WriteLine("Počet nul: {0}", zeros);
+    Console.WriteLine("Počet sudých čísel {0}", evenNumbs);
+    Console.WriteLine("Počet lichých čísel: {0}", oddNumbs);
 
     Console.WriteLine();
     Console.WriteLine("Pro opakování programu stiskněte klávesu a");
